7f3b65e2bf03231ed70d69f92411be17427ca30f
jochensambaer1 <jochen_sambaer@hotmail.com>
2024-10-20 10:50:07 +0200

    leson 2

--- PyTorch Tutorial 01/pythorch_tutorial.py
+++ PyTorch Tutorial 01/pythorch_tutorial.py
@@ -3,3 +3,121 @@ import torch
 x = torch.empty(1)
 
 print(x)
+
+
+
+x = torch.empty(1) # scalar
+print(x)
+x = torch.empty(3) # vector, 1D
+print(x)
+x = torch.empty(2,3) # matrix, 2D
+print(x)
+x = torch.empty(2,2,3) # tensor, 3 dimensions
+#x = torch.empty(2,2,2,3) # tensor, 4 dimensions
+print(x)
+
+
+x = torch.rand(5, 3)
+print(x)
+
+# torch.zeros(size), fill with 0
+# torch.ones(size), fill with 1
+x = torch.zeros(5, 3)
+print(x)
+
+# check size
+print(x.size())
+
+# check data type
+print(x.dtype)
+
+# specify types, float32 default
+x = torch.zeros(5, 3, dtype=torch.float16)
+print(x)
+
+# check type
+print(x.dtype)
+
+# construct from data
+x = torch.tensor([5.5, 3])
+print(x.size())
+
+
+x = torch.tensor([5.5, 3], requires_grad=True)
+
+# Operations
+y = torch.rand(2, 2)
+x = torch.rand(2, 2)
+
+# elementwise addition
+z = x + y
+# torch.add(x,y)
+
+# y.add_(x)
+
+# substraction
+z = x - y
+z = torch.sub(x, y)
+
+# multiplication
+z = x * y
+z = torch.mul(x,y)
+
+# division
+z = x / y
+z = torch.div(x,y)
+
+# Slicing
+x = torch.rand(5,3)
+print(x)
+print(x[:, 0]) # all rows, column 0
+print(x[1, :]) # row 1, all columns
+print(x[1,1]) # element at 1, 1
+
+# Get the actual value if only 1 element in your tensor
+print(x[1,1].item())
+
+# Reshape with torch.view()
+x = torch.randn(4, 4)
+y = x.view(16)
+z = x.view(-1, 8)  # the size -1 is inferred from other dimensions
+# if -1 it pytorch will automatically determine the necessary size
+print(x.size(), y.size(), z.size())
+
+
+a = torch.ones(5)
+print(a)
+
+# torch to numpy with .numpy()
+b = a.numpy()
+print(b)
+print(type(b))
+
+
+a.add_(1)
+print(a)
+print(b)
+
+# numpy to torch with .from_numpy(x)
+import numpy as np
+a = np.ones(5)
+b = torch.from_numpy(a)
+print(a)
+print(b)
+
+# again be careful when modifying
+a += 1
+print(a)
+print(b)
+
+# by default all tensors are created on the CPU,
+# but you can also move them to the GPU (only if it's available )
+if torch.cuda.is_available():
+    device = torch.device("cuda")          # a CUDA device object
+    y = torch.ones_like(x, device=device)  # directly create a tensor on GPU
+    x = x.to(device)                       # or just use strings ``.to("cuda")``
+    z = x + y
+    # z = z.numpy() # not possible because numpy cannot handle GPU tenors
+    # move to CPU again
+    z.to("cpu")       # ``.to`` can also change dtype together!
+    # z = z.numpy()
